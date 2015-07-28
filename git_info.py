e7d2ae595a5e841d4e61df7b3ef7550a92e0353d
Igor Svilenkov <igor@svilenkov.com>
2015-07-28 10:48:39 +0200

    added GitInfo class

---
+++ git_info.py
@@ -0,0 +1,7 @@
+class GitInfo():
+    def __init__(self, user, repo, branch, remote_path, remote_name):
+        self.user = user
+        self.repo = repo
+        self.branch = branch
+        self.remote_path = remote_path
+        self.remote_name = remote_name
