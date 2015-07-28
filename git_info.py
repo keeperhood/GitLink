a7c67bd95424f60b48fbdeac25ddc32503ee8c24
Igor Svilenkov <igor@svilenkov.com>
2015-07-28 17:01:20 +0200

    added project name to GitInfo

--- git_info.py
+++ git_info.py
@@ -1,7 +1,9 @@
 class GitInfo():
-    def __init__(self, user, repo, branch, remote_path, remote_name):
+    def __init__(self, user, repo, branch, remote_path, remote_name, commit, project):
         self.user = user
         self.repo = repo
         self.branch = branch
         self.remote_path = remote_path
         self.remote_name = remote_name
+        self.commit = commit
+        self.project = project
